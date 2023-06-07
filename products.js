2d66044e23f3d771d619aea6c43e22fad5ae5d0c
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-06-07 09:40:42 -0400

    Initial commit

---
+++ products.js
@@ -0,0 +1,24 @@
+fetch("products.json")
+.then(function(response){
+   return response.json();
+})
+.then(function(products){
+   let output = "";
+   // if( categoryTitle)
+   for(let product of products){
+      
+      output += `
+      <div class="product tab-content">
+      <img src="${product.image}">
+      <p class="title">${product.title}</p>
+      <p class="description">${product.description}</p>
+      <p class="price">
+         <span>${product.price}</span>
+      </p>
+   </div>
+      `;
+   }
+   document.querySelector(".products").innerHTML = output;
+});
+
+
